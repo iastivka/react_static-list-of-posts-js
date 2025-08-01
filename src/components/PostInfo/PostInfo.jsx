9977705ea84a7306f61575fd75fe3ea5946b2195
Andrii Poliukh <andriypoliukh@gmail.com>
2025-08-01 16:02:00 +0300

    Solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,18 @@
-export const PostInfo = () => <>Put the post here</>;
+import './PostInfo.scss';
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const PostInfo = ({ title, body, user, comments }) => (
+  <div className="PostInfo">
+    <h2 className="PostInfo__title">{title}</h2>
+    <p className="PostInfo__body">{body}</p>
+
+    <UserInfo name={user.name} email={user.email} />
+
+    <div className="PostInfo__comments">
+      {comments.map(({ id, name, email, body: commentBody }) => (
+        <CommentInfo key={id} name={name} email={email} body={commentBody} />
+      ))}
+    </div>
+  </div>
+);
