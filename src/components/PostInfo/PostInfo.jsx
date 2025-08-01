1877fbc5d0cd10a70855308e2d055f6d433949f1
Andrii Poliukh <andriypoliukh@gmail.com>
2025-08-01 16:55:16 +0300

    fixed

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,18 +1,14 @@
 import './PostInfo.scss';
 import { UserInfo } from '../UserInfo/UserInfo';
-import { CommentInfo } from '../CommentInfo/CommentInfo';
+import { CommentList } from '../CommentList/CommentList';
 
 export const PostInfo = ({ title, body, user, comments }) => (
   <div className="PostInfo">
     <h2 className="PostInfo__title">{title}</h2>
     <p className="PostInfo__body">{body}</p>
 
-    <UserInfo name={user.name} email={user.email} />
+    {user && <UserInfo name={user.name} email={user.email} />}
 
-    <div className="PostInfo__comments">
-      {comments.map(({ id, name, email, body: commentBody }) => (
-        <CommentInfo key={id} name={name} email={email} body={commentBody} />
-      ))}
-    </div>
+    <CommentList comments={comments || []} />
   </div>
 );
